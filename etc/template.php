3b9de024e792a241e072426c296d3aa468ca6f11
Lukas Bestle <mail@lukasbestle.com>
2016-08-03 17:23:53 +0200

    Fix #2: Typo in module template

--- etc/template.php
+++ etc/template.php
@@ -3,7 +3,7 @@
 namespace Kirby\Modules;
 
 // Redirect to the page where the module appears
-if($page->parent()->uid() === Modules::uid()) {
+if($page->parent()->uid() === Modules::parentUid()) {
 	go($page->parent()->parent());
 } else {
 	go($page->parent());
